820b91c3e3430135c8a0d72c268491d25b396e3b
Cyzo50218 <myfavoritemappingswar@gmail.com>
2024-09-04 21:36:27 +0800

    update app.js

--- client/subscription/daily/checkout/app.js
+++ client/subscription/daily/checkout/app.js
@@ -110,8 +110,7 @@ fetch('/subscription/weekly/api/generateplan')
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
-                    plan_id: productId,
-                    vault: true // Save payment method for future use
+                    plan_id: productId
                 }),
             })
             .then(response => response.json())
